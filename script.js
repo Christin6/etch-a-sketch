1d6fc696620b261eaf8909578be0abd7ebc3c775
Christin6 <94294443+Christin6@users.noreply.github.com>
2024-03-19 20:57:24 +0700

    add change size feature

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 const container = document.querySelector(".container");
 
-let size = 30;
+function createSketch(size) {
+    for (let i=0; i<size**2; i++) {
+        let box = document.createElement("div");
+        box.className = "item";
+        box.style.height = `calc(100% / ${size})`;
+        box.style.width = `calc(100% / ${size})`;
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black";
+        });
+        container.appendChild(box);
+    }
+}
 
-for (let i=0; i<size**2; i++) {
-    let box = document.createElement("div");
-    box.className = "item";
-    box.style.height = `calc(100% / ${size})`;
-    box.style.width = `calc(100% / ${size})`;
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "black";
-    });
-    container.appendChild(box);
+function createDefaultSketch() {
+    const defaultSize = 16;
+    createSketch(defaultSize);
 }
 
 function resetContainer() {
-    const boxes = document.querySelectorAll(".item");
-    let boxesArray = Array.from(boxes);
     boxesArray.map((element) => {
         element.style.backgroundColor = "#dcdcdc";
     });
 }
+
+function changeContainerSize() {
+    let newSize = parseInt(prompt("Input size (1-100):"));
+    if (newSize > 0 && newSize <= 100) {
+        container.replaceChildren();
+        createSketch(newSize);
+        boxesArray.map((element) => {
+            console.log("test");
+            element.style.height = `calc(100% / ${newSize})`;
+            element.style.width = `calc(100% / ${newSize})`;
+        });
+    } else {alert("Something is wrong with your input")}
+}
+
+createDefaultSketch();
+const boxes = document.querySelectorAll(".item");
+const boxesArray = Array.from(boxes);
