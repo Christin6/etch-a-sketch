ddaffc2c3d8fc2f62c91f74db2bf7c570ffe54c2
Christin6 <94294443+Christin6@users.noreply.github.com>
2024-03-20 08:53:06 +0700

    add color change functionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const container = document.querySelector(".container");
-
 function createSketch(size) {
     for (let i=0; i<size**2; i++) {
         let box = document.createElement("div");
@@ -37,6 +35,57 @@ function changeContainerSize() {
     } else {alert("Something is wrong with your input")}
 }
 
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+
+
+function randomRGBColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return `rgb(${r} ${g} ${b})`;
+}
+
+function changeColor(element, color) {
+    element.style.backgroundColor = color;
+}
+
+function changePenColor(color) {
+    if (color == "rainbow") {
+        boxesArray.map((element) => {
+            element.addEventListener("mouseover", function rainbow() {
+                changeColor(element, randomRGBColor());
+            });
+        });
+    } else {
+        boxesArray.map((element) => {
+            element.addEventListener("mouseover", function normal() {
+                changeColor(element, color);
+            });
+        });
+    }
+}
+
+function blue() {
+    changePenColor("blue");
+}
+
+function randomColor() {
+    changePenColor(randomRGBColor());
+}
+
+function rainbowColor() {
+    changePenColor("rainbow");
+}
+
+function changeBackgroundColor(color) {
+    boxesArray.map((element) => {
+        element.addEventListener("mouseover", changeColor(element, color));
+    });
+}
+
+const container = document.querySelector(".container");
 createDefaultSketch();
 const boxes = document.querySelectorAll(".item");
 const boxesArray = Array.from(boxes);
